44865f3ebe0f4a6a87a2bcfb9a7b6f8c4e4d8afa
borting <borting@users.noreply.github.com>
2020-05-02 14:43:51 +0800

    Not cleanup in build.bat
    We don't need to clean up log after every build

--- build.bat
+++ build.bat
@@ -5,4 +5,3 @@ set LATEX_BUILD_CMD=xelatex "-synctex=1 -shell-escape -interaction=nonstopmode -
 bibtex main.aux || true
 %LATEX_BUILD_CMD% %sTARGET%.tex
 %LATEX_BUILD_CMD% %sTARGET%.tex
-del *.aux *.bbl *.blg *.idx *.ind *.lof *.lot *.out *.toc *.acn *.acr *.alg *.glg *.glo *.gls *.ist *.fls *.log *.fdb_latexmk
